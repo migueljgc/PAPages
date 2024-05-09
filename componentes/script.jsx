eecf6ef4c9c545acab7e9eb9980a5d6650f8a6f2
migueljgc <miguelgaviriam.8@gmail.com>
2024-05-09 08:10:11 -0500

    actualizacion

--- componentes/script.jsx
+++ componentes/script.jsx
@@ -7,9 +7,9 @@ export const Script = () => {
     <div className="dropdown-content">
       <div class="App">
         <div className="arriba"><p><span className="username">¡Hola, Admin!</span></p></div>
-        <div className="abajo"><a href="#">
+        <div className="abajo"><a href="/EditarPerfil">
           <i className="fa fa-plus"></i>Editar usuario</a>
-          <a href="#"><i className="fa fa-sign-out"></i>Salir</a></div>
+          <a href="/Login"><i className="fa fa-sign-out"></i>Salir</a></div>
       </div>
 
     </div>
---
+++ src/Pages/Usuarios/Admin/CrearUsuario/CrearUsuario.jsx
@@ -0,0 +1,5 @@
+
+export const CrearUsuario  = () => {
+    return (
+        <div className="crearUsuario"></div>
+    )}
