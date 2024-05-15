d396fa318f893e6305430a0ce99b2c5e1e69b7cd
migueljgc <miguelgaviriam.8@gmail.com>
2024-05-14 21:34:00 -0500

    actualizacion

--- src/Pages/Login and Register/Login.jsx
+++ src/Pages/Login and Register/Login.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import './Login.css';
 import { VscAccount } from "react-icons/vsc";
 import { RiLockPasswordLine } from "react-icons/ri";
-import {useForm} from '../../../hook/useForm'
 import { useNavigate } from 'react-router-dom';
-import {BackGraund} from '../../../componentes/BackGraund'
+import { BackGraund } from '../../../componentes/BackGraund';
+import axios from 'axios';
 
 export const Login = () => {
     const navigate = useNavigate();
@@ -21,7 +21,7 @@ export const Login = () => {
             });
 
             if (response.status === 200) {
-                navigate('/HomePageAdmin', {
+                navigate('/HomePagesAdmin', {
                     replace: true,
                     state: {
                         logged: true,
@@ -36,44 +36,44 @@ export const Login = () => {
             alert('Error al iniciar sesión. Por favor, inténtalo de nuevo.');
         }
     }
-    
+
     return (
         <div className='login'>
             <BackGraund />
             <div className="formLogin" onSubmit={onLogin}>
-            <div className="usa">
-                <div className="usu"></div>
-            </div>
-            <div className="loginForm">
-                <form action="" className="Formlogin">
-                    <h1>Login</h1>
+                <div className="usa">
+                    <div className="usu"></div>
+                </div>
+                <div className="loginForm">
+                    <form action="" className="Formlogin">
+                        <h1>Login</h1>
 
-                    <div className="input-box">
-                        <label htmlFor='usuario'><VscAccount /> Usuario: </label><br />
-                        <input type="text" className='usuario' id='usurio' value={user} onChange={e => setUser(e.target.value)} required />
-                    </div><br />
-                    <div className="input-box">
-                        <label htmlFor=""><RiLockPasswordLine /> Contraseña: </label><br />
-                        <input type="password" className='contrasena' id='contrasena' value={password} onChange={e => setPassword(e.target.value)} required />
-                    </div><br />
-                    <div className="btnIniciarSesion">
-                        <button >
-                            Iniciar Sesión 
-                        </button>
-                    </div><br />
-                    <div className="remember-forgod">
-                        <label > <input type="checkbox" />Recuerdame <label >  </label></label>
-                        <a href="/Recuperacion">  ¿Olvidaste tu contraseña?</a>
-                    </div>
-                    <div className="register">
-                        <p>¿No tienes cuenta aún? <a href="/Registro">Registrar</a></p>
-                    </div>
-                </form>
-            </div>
+                        <div className="input-box">
+                            <label htmlFor='user'><VscAccount /> Usuario: </label><br />
+                            <input type="text" className='usuario' value={user} onChange={e => setUser(e.target.value)} required />
+                        </div><br />
+                        <div className="input-box">
+                            <label htmlFor=""><RiLockPasswordLine /> Contraseña: </label><br />
+                            <input type="password" className='contrasena' value={password} onChange={e => setPassword(e.target.value)} required />
+                        </div><br />
+                        <div className="btnIniciarSesion">
+                            <button type="submit">
+                                Iniciar Sesión 
+                            </button>
+                        </div><br />
+                        <div className="remember-forgod">
+                            <label> <input type="checkbox" />Recuérdame</label>
+                            <a href="/Recuperacion">  ¿Olvidaste tu contraseña?</a>
+                        </div>
+                        <div className="register">
+                            <p>¿No tienes cuenta aún? <a href="/Registro">Registrar</a></p>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-
     );
 };
+
 
 
--- src/Pages/Usuarios/User/CrearPQRS/CrearPQRS.jsx
+++ src/Pages/Usuarios/User/CrearPQRS/CrearPQRS.jsx
@@ -4,8 +4,126 @@ import { BackGraund } from '../../../../../componentes/BackGraund';
 
 export const CrearPQRS = () => {
     return (
-        <div className='VerPQRS'>
-            <BackGraund/>
-            <MenuUser/>
+        <div className='CrearPQRS'>
+            <BackGraund />
+            <MenuUser />
+            <div className="fr">
+                <div className="formu">
+                    <form className='formPQRS'>
+                        <div className="input-box1">
+                            <label htmlFor="nombre">Nombre:</label><br />
+                            <input
+                                type="text"
+                                id="nombre"
+                                name="nombre"
+                            />
+                        </div> <br />
+                        <div className="input-box1">
+                            <label htmlFor="apellido">Apellido:</label><br />
+                            <input
+                                type="text"
+                                id="apellido"
+                                name="apellido"
+                            />
+                        </div > <br />
+                        <div className="input-box1">
+                            <label htmlFor="correo">Correo:</label><br />
+                            <input
+                                type="email"
+                                id="correo"
+                                name="correo"
+                               
+                            />
+                        </div> <br />
+                        <div className="input-box1">
+                            <label htmlFor="numero">Número:</label><br />
+                            <input
+                                type="text"
+                                id="numero"
+                                name="numero"
+                                
+                            />
+                        </div> <br />
+                        <div className="select-box1">
+                            <label htmlFor="rol">Rol:</label><br />
+                            <select
+                                type="text"
+                                id="rol"
+                                name="rol"
+                               >
+
+                                <option value="">Seleccione el rol</option>
+                                <option value="Admin">Administrador</option>
+                                <option value="User">Usuario</option>
+                                <option value="Dependencia">Dependencia</option>
+
+                            </select>
+                        </div> <br />
+                        <div className="input-box1">
+                            <label htmlFor="usuario">Usuario:</label><br />
+                            <input
+                                type="text"
+                                id="usuario"
+                                name="usuario"
+                            />
+                        </div> <br />
+                        <div className="input-box1">
+                            <label htmlFor="contraseña">Contraseña:</label><br />
+                            <input
+                                type="password"
+                                id="contraseña"
+                                name="contraseña"
+                            />
+                        </div> <br />
+                        <div className="input-box1">
+                            <label htmlFor="confirmarContraseña">Confirmar Contraseña:</label><br />
+                            <input
+                                type="password"
+                                id="confirmarContraseña"
+                                name="confirmarContraseña"
+                            />
+                        </div> <br />
+                        <div className="select-box1">
+                            <label htmlFor="tipoPersona">Tipo de Persona:</label><br />
+                            <select
+                                type="text"
+                                id="tipoPersona"
+                                name="tipoPersona">
+
+                                <option value="">Seleccione el tipo</option>
+                                <option value="Natural">Natural</option>
+                                <option value="Juridica">Juridica</option>
+
+                            </select>
+                        </div> <br />
+                        <div className="select-box1">
+                            <label htmlFor="tipoIdentificacion">Tipo de Identificación:</label><br />
+
+                            <select
+                                type="text"
+                                id="tipoIdentificacion"
+                                name="tipoIdentificacion">
+
+                                <option value="">Seleccione Tipo de Identidicacion</option>
+                                <option value="C.C">C.C</option>
+                                <option value="T.I">T.I</option>
+                                <option value="P.A">P.A</option>
+
+                            </select>
+                        </div> <br />
+                        <div className="input-box1">
+                            <label htmlFor="identificacion">Identificación:</label><br />
+                            <input
+                                type="text"
+                                id="identificacion"
+                                name="identificacion"
+                            />
+                        </div><br />
+                        <div className="btnIniciarSesion">
+                            <button type="submit">Registrar</button>
+                        </div><br />
+                    </form>
+                </div>
+            </div>
         </div>)
 }
